9ab4677f816a79921e86643f5d6bec3a8e8bd526
Rein van der Woerd <reinvanderwoerd@me.com>
2017-01-19 13:51:22 +0100

    even better transitions

--- guide.php
+++ guide.php
@@ -21,7 +21,7 @@ class Guide
     function guide_load()
     {
         wp_enqueue_style('guide.css', plugin_dir_url(__FILE__) . 'guide.css');
-        wp_enqueue_script('guide.js', plugin_dir_url(__FILE__) . 'index.js');
+        wp_enqueue_script('guide.js', plugin_dir_url(__FILE__) . 'guide.js');
     }
 }
 
