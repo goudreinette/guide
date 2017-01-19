08a66c895cf6f3e30274e20a1dda84fa8e91e3e9
Rein van der Woerd <reinvanderwoerd@me.com>
2017-01-19 11:49:12 +0100

    Smoot Transitions

--- guide.php
+++ guide.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Guide
  * Plugin URI: http://reinvdwoerd.herokuapp.com
@@ -8,24 +9,20 @@
  * Author URI: http://reinvdwoerd.herokuapp.com
  * License: -
  */
-
 class Guide
 {
-  function __construct()
-  {
-    if (!is_admin())
+    function __construct()
     {
-      add_action('wp_enqueue_scripts', [$this, 'guide_load']);
+        if (!is_admin()) {
+            add_action('wp_enqueue_scripts', [$this, 'guide_load']);
+        }
     }
-  }
 
-  function guide_load ()
-  {
-    wp_enqueue_style('guide.css', plugin_dir_url( __FILE__ ) . 'guide.css');
-    // wp_enqueue_script('jquery.js', plugin_dir_url( __FILE__ ) . 'jquery.js');
-    wp_enqueue_script('velocity.min.js', plugin_dir_url( __FILE__ ) . 'velocity.min.js');
-    wp_enqueue_script('guide.js', plugin_dir_url( __FILE__ ) . 'bundle.js');
-  }
+    function guide_load()
+    {
+        wp_enqueue_style('guide.css', plugin_dir_url(__FILE__) . 'guide.css');
+        wp_enqueue_script('guide.js', plugin_dir_url(__FILE__) . 'index.js');
+    }
 }
 
 new Guide();
--- index.js
+++ index.js
@@ -1,100 +1,96 @@
 /* global $ */
-class ViewModel
-{
-  constructor ()
-  {
-    // DOM reference to mount point
-    this.$root = document.querySelector('#contact-slideout .container')
-    this.appendHTML()
+class Guide {
+    constructor() {
+        // DOM reference to mount point
+        this.$root = document.querySelector('#contact-slideout .container')
+        this.appendHTML()
 
-    // Get references to just created DOM elements
-    this.$toggle = document.querySelector('.contact-menu-link i')
-    this.$back = document.querySelector('#contact-slideout #back')
-    this.$choices = document.querySelectorAll('#contact-slideout h1.choice')
-    this.$icons = document.querySelectorAll('#contact-slideout .choice-icon')
+        // Get references to just created DOM elements
+        this.$toggle = document.querySelector('.contact-menu-link i')
+        this.$back = document.querySelector('#contact-slideout #back')
+        this.$choices = document.querySelectorAll('#contact-slideout h1.choice')
+        this.$icons = document.querySelectorAll('#contact-slideout .choice-icon')
 
-    this.$rows = document.querySelectorAll('#contact-slideout .guide')
-    this.$intro = document.querySelector('#contact-slideout #intro')
+        this.$rows = document.querySelectorAll('#contact-slideout .guide')
+        this.$intro = document.querySelector('#contact-slideout #intro')
 
-    // Event listeners
-    window.toggleGuide = () => $(this.$toggle).trigger('click')
-    window.addEventListener('hashchange', this.onHashChange.bind(this))
-    this.$toggle.addEventListener('click', this.onToggleClick.bind(this))
-    this.$back.addEventListener('click', this.onBackClick.bind(this))
-    for (let i = 0; i < this.$choices.length; i++)
-      this.$choices[i].addEventListener('click', this.onChoiceClick.bind(this))
+        // Event listeners
+        window.toggleGuide = () => $(this.$toggle).trigger('click')
+        window.addEventListener('hashchange', this.onHashChange.bind(this))
+        this.$toggle.addEventListener('click', this.onToggleClick.bind(this))
+        this.$back.addEventListener('click', this.onBackClick.bind(this))
+        for (let i = 0; i < this.$choices.length; i++)
+            this.$choices[i].addEventListener('click', this.onChoiceClick.bind(this))
 
-    // Update for the first time - for when hash !== ''
-    this.update()
-  }
-
-  appendHTML ()
-  {
-    const html = '<button id="back"></button>'
-    this.$root.insertAdjacentHTML('afterbegin', html)
-  }
-
-  onToggleClick ()
-  {
-    location.hash = ''
-  }
-
-  onChoiceClick (event)
-  {
-    location.hash = event.target.getAttribute('data-slug')
-  }
-
-  onBackClick ()
-  {
-    history.back()
-  }
-
-  onHashChange ()
-  {
-    this.update()
-  }
-
-  update ()
-  {
-    // Show/hide grids based on their slug's occurence in location.hash
-    for (let i = 0; i < this.$rows.length; i++)
-    {
-      const slug = this.$rows[i].getAttribute('data-slug')
-      const hidden = location.hash.slice(1) !== slug
-      if (hidden)
-      {
-        $(this.$rows[i]).velocity({opacity: 0, height: 0, scaleY: 0})
-      }
-      else
-      {
-        $(this.$rows[i]).velocity({opacity: 1, height: 'auto', scaleY: 1})
-      }
-    }
-    // Show/hide the #intro based on the content of location.hash
-    if (location.hash !== '')
-    {
-      $(this.$intro).velocity({opacity: 0, height: 0, marginTop: -230})
-      $(this.$back).velocity({opacity: 1, height: 'auto'})
-    }
-    else
-    {
-      $(this.$back).velocity({opacity: 0, height: 0})
-      $(this.$intro).velocity({opacity: 1, height: 'auto', marginTop: 100})
+        // Update for the first time - for when hash !== ''
+        this.show = false
+        this.update()
     }
 
-    // Set first choice active based on location.hash
-    for (let i = 0; i < this.$choices.length; i++)
-      this.$choices[i].classList.toggle('active', location.hash.includes(this.$choices[i].getAttribute('data-slug')))
+    appendHTML() {
+        const html = '<button id="back">Terug</button>'
+        this.$root.insertAdjacentHTML('afterbegin', html)
+    }
 
-    // Change the background color when location.hash includes individueel
-    this.$root.parentNode.classList.toggle('alternate-color', location.hash.includes('individueel'))
+    onToggleClick() {
+        location.hash = ''
+        this.show = !this.show
+        this.update()
+    }
 
-    // Hide icons when location.hash !== ''
-    for (var i = 0; i < this.$icons.length; i++)
-      this.$icons[i].classList.toggle('hidden', location.hash !== '')
-  }
+    onChoiceClick(event) {
+        location.hash = event.target.getAttribute('data-slug')
+    }
+
+    onBackClick() {
+        history.back()
+    }
+
+    onHashChange() {
+        this.update()
+    }
+
+    update() {
+        // Show/hide grids based on their slug's occurence in location.hash
+        for (let i = 0; i < this.$rows.length; i++) {
+            const slug = this.$rows[i].getAttribute('data-slug')
+            const hidden = location.hash.slice(1) !== slug
+            if (hidden) {
+                $(this.$rows[i]).velocity({opacity: 0, height: 0, scaleY: 0})
+            }
+            else {
+                $(this.$rows[i]).velocity({opacity: 1, height: 'auto', scaleY: 1})
+            }
+        }
+        // Show/hide the #intro based on the content of location.hash
+        if (location.hash !== '') {
+            $(this.$intro).velocity({opacity: 0, height: 0, marginTop: -230})
+            $(this.$back).velocity({opacity: 1, height: 30})
+        }
+        else {
+            $(this.$back).velocity({opacity: 0, height: 0})
+            $(this.$intro).velocity({opacity: 1, height: 220, marginTop: 100})
+        }
+
+        // Set first choice active based on location.hash
+        for (let i = 0; i < this.$choices.length; i++)
+            this.$choices[i].classList.toggle('active', location.hash.includes(this.$choices[i].getAttribute('data-slug')))
+
+        // Change the background color when location.hash includes individueel
+        this.$root.parentNode.classList.toggle('alternate-color', location.hash.includes('individueel'))
+
+        /**
+         * Root visibility
+         */
+        debugger
+        this.$root.parentNode.classList.toggle('show', this.show)
+
+        // Hide icons when location.hash !== ''
+        for (var i = 0; i < this.$icons.length; i++)
+            this.$icons[i].classList.toggle('hidden', location.hash !== '')
+    }
 }
 
 if (!window.$) window.$ = window.jQuery
 
-window.onload = () => new ViewModel()
+window.onload = () => new Guide()
